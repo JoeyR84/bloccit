c6feec523b97e1c9fc12758fbdaedf6153f9d758
Joey R <joeyr84@gmail.com>
2017-08-18 14:52:55 -0600

    checkpoint-19

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,37 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each_with_index do |p, i|
+      p.title = 'SPAM' if i%5 == 0
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def edit
+  end
+
+  def create
+  # #9
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+  # #10
+    if @post.save
+  # #11
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+  # #12
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
   end
 end
