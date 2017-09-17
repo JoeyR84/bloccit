7eae42a2c8cc1fcaf0b663e3ddba186ddb12ff9d
Joey R <joeyr84@gmail.com>
2017-09-17 10:59:07 -0600

    assignment 20 w errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :advertisement
+  resources :question
 
+  resources :advertisement
 
   resources :posts
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,22 +2,26 @@ require 'random_data'
 
 # Create Posts
 50.times do
-# #1
   Post.create!(
-# #2
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
   )
 end
 posts = Post.all
 
-# Create Comments
-# #3
 100.times do
   Comment.create!(
-# #4
     post: posts.sample,
     body: RandomData.random_paragraph
+  )
+end
+
+#Create Questions
+100.times do
+  Question.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    resolved: false
   )
 end
 
@@ -27,3 +31,4 @@ Comment.find_or_create_by!(body: "check 16 test comment", post: Post.find_or_cre
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"
