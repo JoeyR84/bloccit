8f727c8b941330f666d6c1ff423e2531dac506d3
Joey R <joeyr84@gmail.com>
2017-10-12 11:22:19 -0600

    checkpoint-30

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,12 +22,15 @@ topics = Topic.all
 
 # Create Posts
 50.times do
-  Post.create!(
+  post = Post.create!(
     user:   users.sample,
     topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
   )
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
+
 end
 posts = Post.all
 
@@ -40,13 +43,13 @@ posts = Post.all
 end
 
 #Create Questions
-100.times do
-  Question.create!(
-    title: RandomData.random_sentence,
-    body: RandomData.random_paragraph,
-    resolved: false
-  )
-end
+# 100.times do
+#   Question.create!(
+#     title: RandomData.random_sentence,
+#     body: RandomData.random_paragraph,
+#     resolved: false
+#   )
+# end
 
 # create sponsored posts
 # 50.times do
@@ -82,6 +85,5 @@ puts "Seed finished"
 puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
-puts "#{SponsoredPost.count} sponsored posts created"
 puts "#{Comment.count} comments created"
-puts "#{Question.count} questions created"
+puts "#{Vote.count} votes created"
