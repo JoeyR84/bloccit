f5fcdab4ec02ae3eb4830db049d9ed3fde948e11
Joey R <joeyr84@gmail.com>
2018-02-03 16:14:20 -0700

    last commit for bloc

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+  def postCount()
+    if @user.posts.count < 1
+      "no posts"
+    else
+      pluralize(@user.posts.count, 'post')
+      pluralize(@user.comments.count, 'comment')
+    end
+    if @user.comments.count < 1
+      'no comments'
+    else
+      pluralize(@user.comments.count, 'comment')
+    end
+  end
 end
